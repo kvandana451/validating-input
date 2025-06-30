2d33d6f7acb01af116b8b62aa0465a0043bd0e68
kvandana451 <kvandana451@gmail.com>
2025-07-01 00:36:31 +0530

    implemented Cross Field Validation

--- app/src/app/shared/identity-revealed.directive.ts
+++ app/src/app/shared/identity-revealed.directive.ts
@@ -1,6 +1,8 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export const identityRevealedValidator = (control: AbstractControl) => {
+export const identityRevealedValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
   const name = control.get('name');
   const alterEgo = control.get('alterEgo');
   return name && alterEgo && name.value === alterEgo.value
